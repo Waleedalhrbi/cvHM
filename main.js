14c8f7d91ec9bbc77254da864015a61c8e00b582
Waleed Alhrbi <waleedalhrbi2001@gmail.com>
2024-09-12 01:34:24 +0300

    creat responsif cv

---
+++ main.js
@@ -0,0 +1,348 @@
+document.getElementById("phon").onmouseover = function() {
+    this.textContent = "+966-505-6064"; 
+};
+document.getElementById("phon").onmouseout = function() {
+    this.textContent = "+123-456-7890"; 
+};
+
+
+document.getElementById("email").onmouseover = function() {
+    this.textContent = "waleed@gmail.com"; 
+};
+document.getElementById("email").onmouseout = function() {
+    this.textContent = "hello@reallygreatsite.com"; 
+};
+
+
+document.getElementById("addres").onmouseover = function() {
+    this.textContent = "336 Riyadh St.,Yarmouk"; 
+};
+document.getElementById("addres").onmouseout = function() {
+    this.textContent = "123 Anywhere St.,Any City"; 
+};
+
+
+document.getElementById("wesit").onmouseover = function() {
+    this.textContent = "www.waleedAlhrbi.com"; 
+};
+document.getElementById("wesit").onmouseout = function() {
+    this.textContent = "www.reallygreatsite.com"; 
+};
+
+
+
+document.getElementById("yearGr1").onmouseover = function() {
+    this.textContent = "2025-2026"; 
+};
+document.getElementById("yearGr1").onmouseout = function() {
+    this.textContent = "2029-2030"; 
+};
+
+
+document.getElementById("NameOfUn1").onmouseover = function() {
+    this.textContent = "King Fahd University"; 
+};
+document.getElementById("NameOfUn1").onmouseout = function() {
+    this.textContent = "WARDIERE UNIVERSITY"; 
+};
+
+
+document.getElementById("academicDegree1").onmouseover = function() {
+    this.textContent = "Master of Mathematics"; 
+};
+document.getElementById("academicDegree1").onmouseout = function() {
+    this.textContent = "Master of Business Managment"; 
+};
+
+
+
+document.getElementById("yearGr2").onmouseover = function() {
+    this.textContent = "2019-2023"; 
+};
+document.getElementById("yearGr2").onmouseout = function() {
+    this.textContent = "2025-2029"; 
+};
+
+document.getElementById("NameOfUn2").onmouseover = function() {
+    this.textContent = "University of Hafar Al-Batin"; 
+};
+document.getElementById("NameOfUn2").onmouseout = function() {
+    this.textContent = "WARDIERE UNIVERSITY"; 
+};
+
+document.getElementById("academicDegree2").onmouseover = function() {
+    this.textContent = "Bachelor of Computer Science and Engineering"; 
+};
+document.getElementById("academicDegree2").onmouseout = function() {
+    this.textContent = "Bachelor of Business"; 
+};
+
+document.getElementById("GPA").onmouseover = function() {
+    this.textContent = "GPA: 4.0 / 4.0"; 
+};
+document.getElementById("GPA").onmouseout = function() {
+    this.textContent = "GPA: 3.8 / 4.0"; 
+};
+
+
+
+
+
+document.getElementById("skill1").onmouseover = function() {
+    this.textContent = "Crisis Management"; 
+};
+document.getElementById("skill1").onmouseout = function() {
+    this.textContent = "Project Management"; 
+};
+
+document.getElementById("skill2").onmouseover = function() {
+    this.textContent = "Negotiation and Mediation"; 
+};
+document.getElementById("skill2").onmouseout = function() {
+    this.textContent = "Public Relations"; 
+};
+
+
+document.getElementById("skill3").onmouseover = function() {
+    this.textContent = "Financial Analysis"; 
+};
+document.getElementById("skill3").onmouseout = function() {
+    this.textContent = "Teamwork"; 
+};
+
+
+document.getElementById("skill4").onmouseover = function() {
+    this.textContent = "Strategic Thinking"; 
+};
+document.getElementById("skill4").onmouseout = function() {
+    this.textContent = "Time Management"; 
+};
+
+
+document.getElementById("skill5").onmouseover = function() {
+    this.textContent = "Quality Assurance"; 
+};
+document.getElementById("skill5").onmouseout = function() {
+    this.textContent = "Leadership"; 
+};
+
+
+document.getElementById("skill6").onmouseover = function() {
+    this.textContent = "Creativity and Innovation"; 
+};
+document.getElementById("skill6").onmouseout = function() {
+    this.textContent = "Effective Communication"; 
+};
+
+
+document.getElementById("skill7").onmouseover = function() {
+    this.textContent = "Data Analysis"; 
+};
+document.getElementById("skill7").onmouseout = function() {
+    this.textContent = "Critical Thinking"; 
+};
+
+
+
+
+document.getElementById("langu1").onmouseover = function() {
+    this.textContent = "Italian (Fluent)"; 
+};
+document.getElementById("langu1").onmouseout = function() {
+    this.textContent = "English (Fluent)"; 
+};
+
+document.getElementById("langu2").onmouseover = function() {
+    this.textContent = "Portuguese (Intermediate)"; 
+};
+document.getElementById("langu2").onmouseout = function() {
+    this.textContent = "French (Fluent)"; 
+};
+
+document.getElementById("langu3").onmouseover = function() {
+    this.textContent = "Dutch (Basics)"; 
+};
+document.getElementById("langu3").onmouseout = function() {
+    this.textContent = "German (Basics)"; 
+};
+
+document.getElementById("langu4").onmouseover = function() {
+    this.textContent = "Arabic (Intermediate)"; 
+};
+document.getElementById("langu4").onmouseout = function() {
+    this.textContent = "Spanish (Intermediate)"; 
+};
+
+
+
+
+
+
+
+document.querySelector(".name h2").onmouseover = function() {
+    this.innerHTML = "WALEED <span>ALHRBI</span>";
+};
+
+document.querySelector(".name h2").onmouseout = function() {
+    this.innerHTML = "RICHARD <span>SANCHEZ</span>";
+};
+
+document.getElementById("spcil").onmouseover = function() {
+    this.textContent = "Computer Science and Engineering";
+};
+
+document.getElementById("spcil").onmouseout = function() {
+    this.textContent = "MARKETING MANAGER";
+};
+
+
+document.getElementById("profile").onmouseover = function() {
+    this.textContent = "My name is Waleed Al-Harbi, a Computer Science and Engineering graduate with a passion for learning and exploring new things in the tech field. I am highly driven to become a skilled Full Stack Developer, continually seeking opportunities to grow and enhance my expertise. Currently, I am looking to deepen my understanding through the Tuwaiq JavaScript Bootcamp, which I believe will provide me with the tools and knowledge to excel in full-stack development. My focus is on mastering both front-end and back-end technologies to create efficient and innovative solutions."; 
+};
+document.getElementById("profile").onmouseout = function() {
+    this.textContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur dolor voluptatibus hic maiores, laboriosam quibusdam eos labore cum, aliquid similique harum vitae eius vel aut! Deserunt tempora explicabo voluptas repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque doloremque odio consequuntur laudantium sit beatae ducimus veniam ratione reprehenderit. Architecto tempora, ullam voluptatem quidem repellendus molestiae fugiat! Dolorum, nemo ratione?"; 
+};
+
+
+
+document.getElementById("plas1").onmouseover = function() {
+    this.textContent = "Arentech Solutions (2030 - Present)"; 
+};
+document.getElementById("plas1").onmouseout = function() {
+    this.textContent = "Borcelto Studio (2030 - Present)"; 
+};
+
+
+document.getElementById("spcil1").onmouseover = function() {
+    this.textContent = "Computer Science and Engineering"; 
+};
+document.getElementById("spcil1").onmouseout = function() {
+    this.textContent = "Marketing Manager & Specialist"; 
+};
+
+
+document.getElementById("listWork1").onmouseover = function() {
+    this.innerHTML = `
+        <li>Ensure adherence to coding standards, security protocols, and software architecture best practices.</li>
+        <li>Collaborate with cross-functional teams to align software solutions with business needs.</li>
+        <li>Oversee the software development lifecycle (SDLC) from concept to deployment, ensuring code quality and timely delivery.</li>
+    `;
+};
+
+document.getElementById("listWork1").onmouseout = function() {
+    this.innerHTML = `
+        <li>Develop and execute comprehensive marketing strategies and campaigns that align with the company's goals and objectives.</li>
+        <li>Lead, mentor, and manage a high-performing marketing team, fostering a collaborative and results-driven work environment.</li>
+        <li>Monitor brand consistency across marketing channels and materials.</li>
+    `;
+};
+
+
+
+
+
+document.getElementById("plas2").onmouseover = function() {
+    this.textContent = "Innotech Systems (2025 - 2039)"; 
+};
+document.getElementById("plas2").onmouseout = function() {
+    this.textContent = "Fauget Studio  2025-2029"; 
+};
+
+
+document.getElementById("spcil2").onmouseover = function() {
+    this.textContent = "Computer Science and Engineering"; 
+};
+document.getElementById("spcil2").onmouseout = function() {
+    this.textContent = "Marketing Manager & Specialist"; 
+};
+
+
+document.getElementById("listWork2").onmouseover = function() {
+    this.innerHTML = `
+    <li>Lead the development of complex software projects, ensuring efficient resource allocation and optimization of performance.</li>
+    <li>Oversee the integration of new technologies and tools into existing software systems.</li>
+    <li>Conduct code reviews and maintain quality control across all stages of development.</li>
+    <li>Collaborate with product managers and stakeholders to identify customer needs and develop tailored solutions.</li>
+    <li>Maintain consistency in software design and implementation across different projects.</li>
+    `;
+};
+
+document.getElementById("listWork2").onmouseout = function() {
+    this.innerHTML = `
+        <li>Create and manage the marketing budget, ensuring efficient allocation of resources and optimizing ROI.</li>
+        <li>Oversee market research to identify emerging trends,customer needs, and competitor strategies.</li>
+        <li>Monitor brand consistency across marketing channels and materials.</li>
+    `;
+};
+
+
+document.getElementById("plas3").onmouseover = function() {
+    this.textContent = "Devora Labs (2024 - 2025)"; 
+};
+document.getElementById("plas3").onmouseout = function() {
+    this.textContent = "Studio Shodwe 2024-2025"; 
+};
+
+
+document.getElementById("spcil3").onmouseover = function() {
+    this.textContent = "Computer Science and Engineering"; 
+};
+document.getElementById("spcil3").onmouseout = function() {
+    this.textContent = "Marketing Manager & Specialist"; 
+};
+
+
+document.getElementById("listWork3").onmouseover = function() {
+    this.innerHTML = `
+    
+    <li>Design, develop, and maintain software applications in collaboration with cross-functional teams.</li>
+    <li>Implement and test new features, debugging and resolving issues to ensure seamless functionality.</li>`;
+};
+
+document.getElementById("listWork3").onmouseout = function() {
+    this.innerHTML = `
+        <li>Develop and maintain strong relationships with partners, agencies,and vendors to support marketing initiatives.</li>
+        <li>Monitor and maintain brand consistency across all marketing channels and materials.</li>
+    `;
+};
+
+
+
+
+
+document.getElementById("referenceinfo1").onmouseover = function() {
+    this.innerHTML = `
+        <li> <h4>Ahmed Alanzi</h4></li>
+        <li>SDAIA inc. / CTO</li>
+        <li>phone: 324-557-8880</li>
+        <li>Email: Ahmed@reallygreatsite.com</li>
+    `;
+};
+document.getElementById("referenceinfo1").onmouseout = function() {
+    this.innerHTML = `
+        <li> <h4>Estelle Darcy</h4></li>
+        <li>Wardiere inc. / CTO</li>
+        <li>phone: 123-456-7890</li>
+        <li>Email: hello@reallygreatsite.com</li>
+    `;
+};
+
+
+
+document.getElementById("referenceinfo2").onmouseover = function() {
+    this.innerHTML = `
+        <li> <h4>Khild Alhrbi</h4></li>
+        <li>Armco inc. / CTO</li>
+        <li>phone: 112-667-9999</li>
+        <li>Email: Khild@reallygreatsite.com</li>
+    `;
+};
+document.getElementById("referenceinfo2").onmouseout = function() {
+    this.innerHTML = `
+        <li> <h4>Harper Richard</h4></li>
+        <li>Wardiere inc. / CTO</li>
+        <li>phone: 123-456-7890</li>
+        <li>Email: hello@reallygreatsite.com</li>
+    `;
+};
+
